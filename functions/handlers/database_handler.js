96f1661030e18e38e29d5e9caa6defeb8f823612
Adrian Crespo Ames <Adrian_Ames@outlook.com>
2020-10-14 18:51:25 -0500

    Service y Controller

--- functions/handlers/database_handler.js
+++ functions/handlers/database_handler.js
@@ -12,22 +12,40 @@ class Database {
         this.database.doc(pathToDocument).update(documentObject);
     }
 
-    async getDocument(collection, document) {
-        const docRef = this.database.collection(collection).doc(document);
+    async getDocument(document) {
+        const docRef = this.database.doc(document);
 
         const query = await docRef.get()
         .then(doc => {
             if (!doc.exists) {
-                console.log('No such document!');
+                console.log('No such document!', document);
                 return 'Not Found'
-            } 
+            }
             return doc.data();
         })
         .catch(err => {
-            console.log('Error getting document', err);
+            console.log('Error getting document', document, err);
         });
 
         return query;
+    }
+
+    async getFromUser(collection, userId) {
+        const docRef = this.database.collection('users').doc(userId).collection(collection);
+
+        const documents = docRef.get()
+        .then(querySnapshot => {
+            var ids = []
+            querySnapshot.forEach(doc => {
+                ids.push(doc.id);
+            });
+            return ids;
+        })
+        .catch(err => {
+            console.log('Error getting documents in', document, err);
+        });
+
+        return documents;
     }
 }
 
